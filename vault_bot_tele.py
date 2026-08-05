08f756cf69f07cc76fdfea05e9d040229fe320f3
makeherblush <shibadesti@gmail.com>
2026-08-05 23:48:15 +0700

    Update Telegram bot token in vault_bot_tele.py

--- vault_bot_tele.py
+++ vault_bot_tele.py
@@ -15,7 +15,7 @@ from telegram.ext import (
 # ==========================================
 # CONFIGURATION
 # ==========================================
-RAW_TOKEN = os.getenv("TELEGRAM_VAULT_BOT_TOKEN", "8598714545:AAH7tckrLSK7TCt3wL5lSBAX9vIzVqdBLVk")
+RAW_TOKEN = os.getenv("TELEGRAM_VAULT_BOT_TOKEN", "8598714545:AAEoyilbbpN1dmnNKlrvkidkwr4BaX_vbww")
 TOKEN = RAW_TOKEN.strip()
 
 # Path Absolut agar kedua bot membaca file fisik database yang sama persis
